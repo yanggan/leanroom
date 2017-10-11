78d3c0dff80c14206603ce34fb3697465551c2da
yanggan <yg@MBP.local>
2017-10-11 21:03:22 +0800

    added: fake data to front

--- serve/application/model.py
+++ serve/application/model.py
@@ -1,30 +1,95 @@
 #coding:utf-8
 import json
+from cprint import cprint
 
-fake_data ={
-    
-"前端开发":{
-    "Vue.js":{
-        "课程名称":"VUE.JS",
-        "课程数目":20,
-        "课程内容":{
-
-            
-
-        }
+# 分类模拟数据
+fake_category = [
+{
+    'category_id':3213,
+    'category_name':"前端开发",
+    'category_count':12,
+    'category_course':[{
+        'course_id':'121',
+        'course_name':'Vue.js课程',
+        'course_img':r'http://vue-js.org/images/vue-js-what-is-that.jpg'
     },
-    "HTML/CSS":{
-
-    }
-
+    {
+        'course_id':'139',
+        'course_name':'angular.js课程',
+        'course_img':r'http://img2.imgtn.bdimg.com/it/u=359120593,1637693053&fm=27&gp=0.jpg'
+    },
+    ]
 },
-"后端开发":{},
-"Android":{},
-"IOS开发":{},
-"网络安全":{},
-"编程语言":{},
-"大数据":{},
-"数据分析":{},
-"运维开发":{},
+{
+    'category_id':4341,
+    'category_name':"后端开发",
+    'category_count':12,
+    'category_course':[{
+        'course_id':'121',
+        'course_name':'Vue.js课程',
+        'course_img':r'http://vue-js.org/images/vue-js-what-is-that.jpg'
+    },
+    {
+        'course_id':'139',
+        'course_name':'angular.js课程',
+        'course_img':r'http://img2.imgtn.bdimg.com/it/u=359120593,1637693053&fm=27&gp=0.jpg'
+    },
+    {
+        'course_id':'121',
+        'course_name':'python课程',
+        'course_img':r'http://vue-js.org/images/vue-js-what-is-that.jpg'
+    },
+    ]
+},
+
+{'category_id':2131,'category_name':'后端开发'},
+{'category_id':1131,'category_name':'移动开发'},
+{
+    'category_id':9041,
+    'category_name':"大数据",
+    'category_count':12,
+    'category_course':[{
+        'course_id':'121',
+        'course_name':'Vue.js课程',
+        'course_img':r'http://vue-js.org/images/vue-js-what-is-that.jpg'
+    },
+    {
+        'course_id':'139',
+        'course_name':'angular.js课程',
+        'course_img':r'http://img2.imgtn.bdimg.com/it/u=359120593,1637693053&fm=27&gp=0.jpg'
+    },
+    {
+        'course_id':'121',
+        'course_name':'python课程',
+        'course_img':r'http://vue-js.org/images/vue-js-what-is-that.jpg'
+    },
+    ]
+},
+]
+
+
+# 分类模拟数据
+
+fake_course_date = {
+    
+    "course_category":'前端开发',
+    'course_id':'121',
+    'course_name':'Vue.js课程',
+    'course_count':12,
+    'course_img':r'http://vue-js.org/images/vue-js-what-is-that.jpg',
+    'course_data':[
+
+        {'resource_id':1,'resource_name':"vue.js入门套课","update_time":'2017.9.11','resource_addr':"http://pan.baidu.com/dad3e",'resource_passwd':"AED3"},
+        {'resource_id':1,'resource_name':"vue.js入门套课","update_time":'2017.9.11','resource_addr':"http://pan.baidu.com/dad3e",'resource_passwd':"AED3"},
+        {'resource_id':1,'resource_name':"vue.js入门套课","update_time":'2017.9.11','resource_addr':"http://pan.baidu.com/dad3e",'resource_passwd':"AED3"},
+        {'resource_id':1,'resource_name':"vue.js入门套课","update_time":'2017.9.11','resource_addr':"http://pan.baidu.com/dad3e",'resource_passwd':"AED3"},
+        {'resource_id':1,'resource_name':"vue.js入门套课","update_time":'2017.9.11','resource_addr':"http://pan.baidu.com/dad3e",'resource_passwd':"AED3"},
+    
+    ]
+
 
 }
+
+if __name__ == "__main__":
+
+    pass
--- serve/application/view.py
+++ serve/application/view.py
@@ -3,18 +3,28 @@ from application import app
 from flask import Flask,request,render_template
 from flask_restful import Resource, Api, abort, reqparse
 
+
+# 避免中文传给jinja2时候报错
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 # 路由管理
+
+from model import *
 
 
 # 首页
 @app.route('/')
 @app.route('/index')
-def index():
-    return render_template("course.html")
-
 @app.route('/course')
-def course():
-    return render_template("course.html")
+def index():
+
+    return render_template("course.html",course=fake_category)
+
+# @app.route('/course')
+# def course():
+#     return render_template("course.html")
 
 
 # 课程详情页
@@ -23,7 +33,7 @@ def course_detail(course_id):
     # 生成数据
 
     # 返回给
-    return render_template("course_detail.html",course_id=course_id)
+    return render_template("course_detail.html",course_data=fake_course_date)
 
 # 通过激活码激活课程
 @app.route("/act")
