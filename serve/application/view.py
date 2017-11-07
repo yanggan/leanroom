6d4fe319f79f81d664817cc55c6d630cbc710d51
yanggan <im@yanggan.me>
2017-11-07 18:53:28 +0800

    v0.1.11.72 mobile course info can use

--- serve/application/view.py
+++ serve/application/view.py
@@ -230,7 +230,70 @@ def mobile_course_detail(course_id):
     passwd_data = Data_Processor.get_passwd_data(course_data)
     is_free = Data_Processor.get_course_free_status(course_data)
 
-    
-    return render_template('/mobile/m_course_detail.html',course_data=course_data,passwd_dict=None,dev_data=dev_data)
-    pass
+
+    if request.method == "GET":
+
+        # 3种情况，1、第一次访问，2、输入兑换码后，重定向访问，这时候需要返回带密码页面
+        # 0、限免课程，直接返回数据
+        # 1、读取用户的cookies和session，把兑换码拿出来，匹配是否是这门课程的对缓慢
+        # 2、如果是这门课程的兑换码，则返回带提取密码数据的页面
+        # 3、如果不是，则返回普通的页面
+        # 判断session,如果有key,value就不用验证了
+        if is_free == True:
+            
+            return render_template("/mobile/m_course_detail.html",course_data=course_data,passwd_dict=passwd_data,dev_data=dev_data)
+        
+        elif session.get('course_'+str(course_id)) != None:
+
+            user_input_key =  session.get('course_'+str(course_id))
+            print user_input_key
+
+            resp = make_response( \
+                render_template("/mobile/m_course_detail.html",course_data=course_data,passwd_dict=passwd_data,dev_data=dev_data)
+                )
+            return resp
+
+        # 判断cookies,需要重新验证vaule验证码是否合法
+        elif request.cookies.get('course_'+ str(course_id)) != None: #有这个课程的cookies
+            
+            cookie_course_key = request.cookies.get('course_'+ str(course_id))
+            print cookie_course_key 
+            verity_result_dict = act.verify_act(course_id,cookies_course_key) #判断code是否有效
+            if verity_result_dict['flag'] == True:
+
+                flash(verity_result_dict['status'])
+                resp = make_response(\
+                    render_template("/mobile/m_course_detail.html",course_data=course_data,passwd_dict=course_data.get('passwd_dict'),dev_data=dev_data)
+                    )
+                return resp
+        else:
+            # 第一次访问
+            return render_template("mobile/m_course_detail.html",course_data=course_data,passwd_dict=None,dev_data=dev_data)
+
+    elif request.method == "POST":
+        # 这是个用户输入兑换码后提交到后台来的数据
+        # 1、获取兑换码，判断是否有效，是对应课程的兑换码[ 课程 + 兑换码 ]
+        # 2、如果有效，则保持兑换码到用户的cookies
+        # 3、把兑换码放到session中
+        # 4、返回重定向，让浏览器get访问本链接
+
+        user_input_key = request.form.get('key', '')        
+        verity_result_dict = Data_Processor.verify_actcode(course_id,user_input_key) # 判断是否正确
+
+        if verity_result_dict['flag'] == True: # 成功之后返回
+
+            session['course_'+str(course_id)] = user_input_key # 设置session
+            print session
+            flash(verity_result_dict['status'])
+            redirect_to_course = redirect(url_for('mobile_course_detail',course_id=course_id))
+            resp = make_response(redirect_to_course)
+            resp.set_cookie('course_'+str(course_id),user_input_key) # 设置cookies
+            return resp # 返回response让浏览器重定向Get访问
+
+        else: #验证失败
+
+            flash(verity_result_dict['status'])
+            return redirect(url_for('mobile_course_detail',course_id=course_id))
+
+
 
