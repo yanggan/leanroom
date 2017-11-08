beff91e27bf302b0c65e323cd764641fa15d9b56
yanggan <im@yanggan.me>
2017-11-08 18:49:08 +0800

    v0.1.11.82 mobile rewrite css style

--- serve/application/view.py
+++ serve/application/view.py
@@ -270,7 +270,7 @@ def mobile_course_detail(course_id):
                 return resp
         else:
             # 第一次访问
-            return render_template("mobile/m_course_detail.html",course_data=course_data,passwd_dict=None,dev_data=dev_data)
+            return render_template("/mobile/m_course_detail.html",course_data=course_data,passwd_dict=None,dev_data=dev_data)
 
     elif request.method == "POST":
         # 这是个用户输入兑换码后提交到后台来的数据
@@ -298,4 +298,39 @@ def mobile_course_detail(course_id):
             return redirect(url_for('mobile_course_detail',course_id=course_id))
 
 
+# 通过激活码激活课程
+@app.route("/m/act",methods=['POST','GET'])
+def mobile_course_activete():
+    # pass
+    if request.method == 'GET':
+        return render_template('/mobile/m_act.html')
+    elif request.method == 'POST':
+        user_input_act = request.form.get('act_code',None) 
+        print user_input_act
 
+        # 输入空的验证码
+        if user_input_act == None or user_input_act == '':
+            print "没有表单数据"
+            flash(u'请输入4位数字兑换码', 'act_error')
+            result_dict = {'flag':False,'status':"actcode is empty",'course_data':None,'error_code':900}
+            return render_template("mobile/act.html",result_dict=result_dict)
+
+        user_input_act = re.search(r'\d{4}',user_input_act).group()  
+        # 返回 {'flag':True,'status':"find actcode and course succeed",'course_data':course_data,'error_code':200}
+        result_dict = Data_Processor.act_verify(user_input_act)
+        
+        # 成功
+        if result_dict.get('flag') == True: #验证成功
+            course_id = str(result_dict.get('course_data').get('course_id'))
+            session['course_'+ course_id ] = user_input_act # 设置session
+            resp = make_response(redirect(url_for('/mobile/m_course_detail',course_id=course_id)))
+            resp.set_cookie('course_'+course_id,user_input_act) # 设置cookies
+            return resp # 返回response让浏览器重定向Get访问
+        # 兑换失败
+        flash(u'兑换码错误,请核对后重新输入', 'act_error')
+        return render_template("/mobile/act.html",result_dict=result_dict)
+
+
+@app.route("/m/my",methods=['POST','GET'])
+def mobile_my():
+    return render_template('/mobile/m_my.html')
